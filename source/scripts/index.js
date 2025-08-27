2b4705623e0caf10742fe81372ec133ab76d893c
Far4ich <79937711+Far4ich@users.noreply.github.com>
2025-08-27 14:54:01 +0300

    Правки после ревью

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,27 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const catalogItems = document.querySelectorAll('.catalog__item');
+  const catalogItems = document.querySelectorAll('.room-card');
 
   catalogItems.forEach((item) => {
-    const button = item.querySelector('.catalog__item-button');
+    const button = item.querySelector('.room-card__button');
 
     button.addEventListener('click', () => {
-      if (item.classList.contains('catalog__item--reserved')) {
+      if (item.classList.contains('room-card--reserved')) {
         return;
       }
-      item.classList.add('catalog__item--selected');
+      item.classList.add('room-card--selected');
     });
 
     item.addEventListener('mouseleave', () => {
-      if (item.classList.contains('catalog__item--selected')
-        && !item.classList.contains('catalog__item--reserved')) {
-        item.classList.add('catalog__item--reserved');
-        item.classList.remove('catalog__item--selected');
+      if (item.classList.contains('room-card--selected')) {
+        item.classList.add('room-card--reserved');
+        item.classList.remove('room-card--selected');
       }
     });
 
     item.addEventListener('click', () => {
-      if (item.classList.contains('catalog__item--reserved')) {
-        item.classList.remove('catalog__item--reserved');
+      if (item.classList.contains('room-card--reserved')) {
+        item.classList.remove('room-card--reserved');
       }
     });
   });
